2d1f6b3466c0d1e17ad48fdefe5673b9b0d99305
Carl Hallén Jansson <callehj@hotmail.se>
2020-05-06 12:05:00 +0200

    Create apollo client

---
+++ src/util/client.js
@@ -0,0 +1,19 @@
+import ApolloClient from "apollo-boost";
+import Cookie from "js-cookie";
+
+const GUEST_KEY = process.env.FAUNADB_GUEST_KEY;
+
+const client = new ApolloClient({
+	uri: "https://graphql.fauna.com/graphql",
+	fetch,
+	request: operation => {
+		operation.setContext({
+			headers: {
+				"Authorization": `Bearer ${Cookie.get("secret") ? Cookie.get("secret") : GUEST_KEY}`,
+				"X-Schema-Preview": "partial-update-mutation"
+			}
+		})
+	}
+})
+
+export default client;
