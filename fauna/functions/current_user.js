5ee0e46550d94daf4ed1fbd63061a12ae08cce80
Carl Hallén Jansson <callehj@hotmail.se>
2020-05-06 12:13:31 +0200

    Add query and function for getting current user

---
+++ fauna/functions/current_user.js
@@ -0,0 +1,13 @@
+const { query } = require("faunadb");
+const { Query, Lambda, Identity, Get } = query;
+
+module.exports = {
+	name: "current_user",
+	body: 
+	Query(
+		Lambda(
+			[],
+			Get(Identity())
+		)
+	)
+}
