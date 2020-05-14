a240f09ca08f5d23e619c775836ba484f9cf53cf
Carl Hallén Jansson <callehj@hotmail.se>
2020-05-14 18:47:54 +0200

    Display login button in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import auth from "../../util/Auth";
 
 // Styles
 import styles from "./footer.module.css";
@@ -7,33 +8,78 @@ import styles from "./footer.module.css";
 import SvgProfile from "../../images/icons/profile.svg";
 import SvgHome from "../../images/icons/home.svg";
 import SvgSaved from "../../images/icons/saved.svg";
+import { useEffect } from "react";
 
 function Footer(){
 	return(
 		<footer className={styles.footer}>
 			<nav>
-				<Link href="/profile">
-					<a>
-						<SvgProfile alt="Profile" />
-						Profile
-					</a>
-				</Link>
-
-				<Link href="/">
-					<a>
-						<SvgHome alt="Home" />
-						Home
-					</a>
-				</Link>
-
-				<Link href="/saved">
-					<a>
-						<SvgSaved alt="Saved" />
-						Saved
-					</a>
-				</Link>
+				{
+					auth.user.loggedIn
+						? <LoggedInView />
+						: <LoggedOutView />
+				}
 			</nav>
 		</footer>
+	);
+}
+
+function LoggedOutView(){
+	useEffect( () => {
+		const loadSignIn = () => {
+			if(!auth.user.loggedIn && auth.google)
+				gapi.load("signin2", () => {
+					gapi.signin2.render("footerLoginButton", {
+						onsuccess: auth.signIn.bind(auth)
+					});
+				});
+		};
+
+		if(auth.google)
+			loadSignIn();
+
+		if(!auth.google)
+			setTimeout(loadSignIn, 1000);
+	});
+
+	return(
+		<>
+			<Link href="/">
+				<a>
+					<SvgHome alt="Home" />
+						Home
+				</a>
+			</Link>
+
+			<div id="footerLoginButton"></div>
+		</>
+	);
+}
+
+function LoggedInView(){
+	return(
+		<>
+			<Link href="/profile">
+				<a>
+					<SvgProfile alt="Profile" />
+						Profile
+				</a>
+			</Link>
+
+			<Link href="/">
+				<a>
+					<SvgHome alt="Home" />
+						Home
+				</a>
+			</Link>
+
+			<Link href="/saved">
+				<a>
+					<SvgSaved alt="Saved" />
+						Saved
+				</a>
+			</Link>
+		</>
 	);
 }
 
