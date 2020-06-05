ec4ca13b00a7c88a66dd3922ed2d177f2ccbd925
Carl Hallén Jansson <callehj@hotmail.se>
2020-06-05 17:23:33 +0200

    Remove prompt from saved page

--- src/pages/saved.js
+++ src/pages/saved.js
@@ -5,14 +5,12 @@ import gql from "graphql-tag";
 import { useRouter } from "next/router";
 
 // Semantic ui
-import { Loader, Modal, Button } from "semantic-ui-react";
+import { Loader } from "semantic-ui-react";
 
 // Styles
 import styles from "../styles/saved.module.css";
-import { useEffect, useContext, useState } from "react";
+import { useEffect, useContext } from "react";
 import UserContext from "../contexts/userContext";
-
-import PWAContext from "../contexts/pwaContext";
 
 const USERS_TENDERS = gql`
 query($id: ID!){
@@ -52,8 +50,6 @@ function SavedTenders(){
 	if(typeof window === "undefined") return"";
 	const user = useContext(UserContext);
 	const router = useRouter();
-	const{ prompt, promptToInstall } = useContext(PWAContext);
-	const[showPrompt, setShowPrompt] = useState(false);
 
 	if(!user.loggedIn) {
 		router.replace("/");
@@ -70,22 +66,10 @@ function SavedTenders(){
 		: (user ? user.savedTenders.data : []);
 	const tenders = allTenders.filter( tender => user.savedTenders.data.map( t => t._id).includes(tender._id));
 
-	const onInstall = () => {
-		promptToInstall();
-		setShowPrompt(false);
-	};
-
 	useEffect( () => {
 		if(error)
 			console.error(error);
 	});
-
-	useEffect( () => {
-		if(user.loggedIn && tenders.length > 0 && prompt){
-			setShowPrompt(true);
-			console.log("Show prompt");
-		}
-	}, [prompt]);
 
 	if(loading)
 		return<Loader active />;
@@ -119,21 +103,6 @@ function SavedTenders(){
 						/>
 					))
 				}
-				<Modal
-					open={showPrompt}
-					size="small"
-					closeIcon
-					closeOnDimmerClick
-					closeOnEscape
-					onClose={ () => { setShowPrompt(false); }}
-				>
-					<Modal.Header>Add Tendersearch.in to your homescreen</Modal.Header>
-					<Modal.Content>
-						<p>It looks like your getting use out of our app, do you wanna add to your homescreen?</p>
-						<Button primary onClick={onInstall}>Add to homescreen</Button>
-					</Modal.Content>
-
-				</Modal>
 			</div>
 		);
 }
