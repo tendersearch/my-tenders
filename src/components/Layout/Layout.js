903dc3f2f82a2959c7b007b2acad84a79ea206bf
Carl Hallén Jansson <callehj@hotmail.se>
2020-06-06 10:58:55 +0200

    Add splash screen

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -51,7 +51,20 @@ export default function Layout({ title, description, children, searchIsFocused }
 					<link rel="manifest" href="/manifest.json" />
 					<link href='/icons/icon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
 					<link href='/icons/icon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-					<link rel="apple-touch-icon" href="/icons/icon-144x144.png"></link>
+					<link rel="apple-touch-icon" href="/icons/icon-144x144.png" />
+
+					<meta name="apple-mobile-web-app-capable" content="yes" />
+					<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+
+					<link rel="apple-touch-startup-image" href="/images/splash/launch-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+					<link rel="apple-touch-startup-image" href="/images/splash/launch-750x1294.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+					<link rel="apple-touch-startup-image" href="/images/splash/launch-1242x2148.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
+					<link rel="apple-touch-startup-image" href="/images/splash/launch-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
+					<link rel="apple-touch-startup-image" href="/images/splash/launch-1536x2048.png" media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)" />
+					<link rel="apple-touch-startup-image" href="/images/splash/launch-1668x2224.png" media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)" />
+					<link rel="apple-touch-startup-image" href="/images/splash/launch-2048x2732.png" media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)" />
+					<meta name="apple-mobile-web-app-title" content="Tendersearch" />
+
 					<meta name="theme-color" content={themeColor}/>
 
 					<link rel="preconnect" href="https://apis.google.com" />
